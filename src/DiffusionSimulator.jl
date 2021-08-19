b5c139542686982704e0b2d29272af44b21c5896
James <gholamja@cardiff.ac.uk>
2021-08-19 15:13:25 +0100

    Files generated by PkgTemplates

---
+++ src/DiffusionSimulator.jl
@@ -0,0 +1,5 @@
+module DiffusionSimulator
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DiffusionSimulator
+using Test
+
+@testset "DiffusionSimulator.jl" begin
+    # Write your tests here.
+end
